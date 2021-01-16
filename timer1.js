02a00fae12c0b2bd63b17f8b9b8437298361cfd0
Mitra Nami <49569563+MitraNami@users.noreply.github.com>
2021-01-16 17:37:13 -0500

    Update timer1.js

--- timer1.js
+++ timer1.js
@@ -1,15 +1,15 @@
 //will beep after a specified amount of time has passed. The user can specify
 //an unlimited number of alarms using command line arguments
 
-const alarmClock = function(alarms) {
-  for (let alarm of alarms) {
-    if (isNaN(Number(alarm)) || Number(alarm) < 0) {
-      continue;
-    } else {
-      setTimeout(() => console.log('Beep!'), alarm * 1000);
+const makeBeep = function(times) {
+  times.forEach(time => {
+    const t = Number(time);
+    if (typeof t === 'number' && t >= 0) {
+      setTimeout(() => process.stdout.write('.'), t * 1000);
     }
-  }
+  })
 };
 
-const alarms = process.argv.slice(2);
-alarmClock(alarms);
+
+const times = process.argv.slice(2);
+makeBeep(times);
